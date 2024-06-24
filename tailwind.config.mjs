4fe5eb33840b4fb2b5ad930da43c5c91dae9e82a
Edu Iglesias <eiglesiash@gmail.com>
2024-06-24 16:40:35 +0200

    Header and carrousel banner. ok

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,20 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				"silver": "#C7C8C8",
+				"red-custom": "#EC151E",
+				"field-drab": "#564710",
+				"french-blue": "#0088f4",
+				"sea-green": "#008E4A",
+				"walnut-brown": "#736455",
+				"khaki": "#B2A392",
+				"cal-poly-green": "#135226",
+				"bole": "793930"
+			}
+
+		},
 	},
 	plugins: [],
 }
